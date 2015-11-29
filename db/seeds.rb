fbc010a5db2ba9bbb0d9203ce0a5f364aee8c18d
Simone Bierti <sbierti@gmail.com>
2015-11-29 09:01:24 +0100

    added seeds fro menu, delete unused fonts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+MenuItem.create!(label: 'Home', reference: 'home', color: 'red', href: 'home_path')
+MenuItem.create!(label: 'Storia', reference: 'storia', color: 'mustard', href: 'storia_path')
+MenuItem.create!(label: 'Scuola', reference: 'scuola', color: 'green', href: 'scuola_path')
+MenuItem.create!(label: 'Orari', reference: 'orari', color: 'yellow', href: 'orari_path')
+MenuItem.create!(label: 'Servizi', reference: 'servizi', color: 'blue', href: 'home_path')
+MenuItem.create!(label: 'POF', reference: 'pof', color: 'steelblue', href: 'home_path')
+MenuItem.create!(label: 'Progetti', reference: 'projects', color: 'lavender', href: 'projects_path')
+MenuItem.create!(label: 'Documenti', reference: 'documenti', color: 'purple', href: 'home_path')
+MenuItem.create!(label: 'Comunicazioni', reference: 'comunicazioni', color: 'pink', href: 'home_path')
